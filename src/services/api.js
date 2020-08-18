71926e5da8158045bbf709ae8fa6efae2ee499b5
cedricmaglione <cedric.maglione@epitech.eu>
2020-08-18 16:21:10 +0200

    changement adress api

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 export default axios.create({
     
-  baseURL: `http://127.0.0.1:8000`,
+  baseURL: `https://nounoubackend.herokuapp.com`,
     headers: { 
         'Authorization': localStorage.getItem('token') 
         
